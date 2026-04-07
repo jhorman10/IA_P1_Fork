70192b9ff9b83f7389abc9eb7df3379974bd11d0
Jhorman Orozco <jhorman.orozco@sofka.com.co>
2026-04-06 20:41:18 -0500

    fix(test): remove stale eslint disable directives

--- backend/producer/test/src/appointments/appointment-lifecycle.controller.spec.ts
+++ backend/producer/test/src/appointments/appointment-lifecycle.controller.spec.ts
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { Reflector } from "@nestjs/core";
 import { Test, TestingModule } from "@nestjs/testing";
